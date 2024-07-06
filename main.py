8cb2da722e101bbedc9b63529b791798d3f1753c
dagmawiisaiah.dev <dagmawiisaiah.dev@gmail.com>
2024-07-06 23:14:35 +0000

    scrap updated

--- main.py
+++ main.py
@@ -2,17 +2,39 @@ import os
 
 from dotenv import load_dotenv
 import telebot
-
-import scrap
+import azapi
 
 load_dotenv()
 
 API_KEY = os.getenv("API_KEY")
 bot = telebot.TeleBot(API_KEY)
+API = azapi.AZlyrics('google', accuracy=0.8)
+
+# Load environment variables from .env file
+load_dotenv()
+
+@bot.message_handler(commands=['start'])
+def start_message(message):
+    bot.send_message(message.chat.id, "Hello, I'm a bot that can send you lyrics of any song you want. Just type the name of the artist and the name of the song, separated by a comma. For example: <NAME>, Never Gonna Give You Up")
 
 @bot.message_handler(content_types=['text'])
 def send_lyrics(message):
-    lyrics = scrap.Scrap(message.text).get_deposits()
-    bot.send_message(message.chat.id, f"{message.text} \n {lyrics}")
+    message_text = message.text.split(', ')
+    API.artist = message_text[0]
+    API.title = message_text[1]
+    API.getLyrics()
+    lyrics = API.lyrics
+    
+    if len(lyrics) < 30:
+        bot.send_message(message.chat.id, "Sorry, I couldn't find any lyrics for that song.")
+        return
+
+    else:
+        # Construct the message with bold formatting
+        bold_artist_title = f"*{message_text[0].title()} - {message_text[1].title()}*"
+        full_message = f"{bold_artist_title}\n\n{lyrics}"
+
+        # Send the message with Markdown style for bold
+        bot.send_message(message.chat.id, full_message, parse_mode='Markdown')
 
 bot.polling()
